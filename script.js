cc8511a761e0ffe6662d86274c3d22d7259938e1
Holly Ashall <hollyashall@icloud.com>
2022-07-06 16:04:15 +0100

    capitalising first letter

--- script.js
+++ script.js
@@ -4,30 +4,24 @@ const containers =document.querySelector(".card-container")
 import pokemonArray from "./data/pokemon.js";
 
 
-// need to capitalise the first letter of the names
-// function capitalizeWords(pokemonArray) {
-//   return pokemonArray.map(name => {
-//     return name.charAt(0).toUpperCase() + name.substring(1).toLowerCase();
-//   });
-// }
-
+const renderContainers = (pokemonObject) =>{
   
-const pokeContainer = (pokemon) => {
-
-  const nameLowerCase = pokemon.name;
-  upperName = nameLowerCase.charAt(0).toUpperCase() + nameLowerCase.substring(1).toLowerCase
-
+  pokemonArray.forEach(entry =>{
+    containers.innerHTML +=  `<div class="card-container"> 
+    <img class="images" src="${entry.sprite}"> </img>
+   <p>${entry.name.charAt(0).toUpperCase()}${entry.name.slice(1)}(#${entry.id})</p><p> is a ${entry.types}   pokemon</p></p>
+     </div>`
+     
+   })
+   
 }
+
+renderContainers(pokemonArray)
 
 
 // using the forEach to loop through the array and put the pokemon details onto the container
- pokemonArray.forEach(entry =>{
-  containers.innerHTML +=  `<div class="container"> 
-  <img class="images" src="${entry.sprite}"> </img>
- <p>${entry.name}</p><p>${entry.types}</p></p>
-   </div>`
- })
 
+// need to add in the second type of pokemon separated by & when this exists
 
 
 
