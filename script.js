7c68f8d05909fbe7d0e5baff78739f1fc8fa3a44
Holly Ashall <hollyashall@icloud.com>
2022-07-06 14:33:43 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const containers =document.querySelector("card-container")
+
+
+// import * as pokemon from './data'
+// pokemon.pokemonArray;
+
+// for(key in pokemon){
+//   console.log(pokemon[key]);
+// }
+
+import defaultExport from './pokemon.js'
+
