a94acd7f296d2af54f7893745b7f0e0903761c12
Holly Ashall <hollyashall@icloud.com>
2022-07-06 16:17:51 +0100

    added both types of pokemon

--- script.js
+++ script.js
@@ -4,12 +4,23 @@ const containers =document.querySelector(".card-container")
 import pokemonArray from "./data/pokemon.js";
 
 
+// need to add in the second type of pokemon separated by & when this exists
+const pokemonType = (type) =>{
+  if (type.length == 1) {
+    return type[0]
+  } 
+  else {return `${type[0]} & ${type[1]}`}
+    
+}
+
+
+
 const renderContainers = (pokemonObject) =>{
   
   pokemonArray.forEach(entry =>{
     containers.innerHTML +=  `<div class="card-container"> 
     <img class="images" src="${entry.sprite}"> </img>
-   <p>${entry.name.charAt(0).toUpperCase()}${entry.name.slice(1)}(#${entry.id})</p><p> is a ${entry.types}   pokemon</p></p>
+   <p>${entry.name.charAt(0).toUpperCase()}${entry.name.slice(1)}(#${entry.id})</p><p> is a ${pokemonType(entry.types)} pokemon</p></p>
      </div>`
      
    })
@@ -21,7 +32,6 @@ renderContainers(pokemonArray)
 
 // using the forEach to loop through the array and put the pokemon details onto the container
 
-// need to add in the second type of pokemon separated by & when this exists
 
 
 
