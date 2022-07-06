f71490208463aaa311280b61863c6e2e4453ab21
Holly Ashall <hollyashall@icloud.com>
2022-07-06 16:27:36 +0100

    changing size of heading

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ import pokemonArray from "./data/pokemon.js";
 
 
 // need to add in the second type of pokemon separated by & when this exists
+// do a function for pokemonType which I can use in my literal template
 const pokemonType = (type) =>{
   if (type.length == 1) {
     return type[0]
   } 
-  else {return `${type[0]} & ${type[1]}`}
-    
+  else {return `${type[0]} & ${type[1]}`}  
 }
 
 
@@ -20,7 +20,8 @@ const renderContainers = (pokemonObject) =>{
   pokemonArray.forEach(entry =>{
     containers.innerHTML +=  `<div class="card-container"> 
     <img class="images" src="${entry.sprite}"> </img>
-   <p>${entry.name.charAt(0).toUpperCase()}${entry.name.slice(1)}(#${entry.id})</p><p> is a ${pokemonType(entry.types)} pokemon</p></p>
+    <h2>${entry.name.charAt(0).toUpperCase()}${entry.name.slice(1)}</h2>
+   <p class="card__text"> ${entry.name.charAt(0).toUpperCase()}${entry.name.slice(1)}(#${entry.id}) is a ${pokemonType(entry.types)} pokemon</p>
      </div>`
      
    })
